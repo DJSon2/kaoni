651690ecf2994e717ae645d3ce528f71ece37e19
DONGJIN\손동진 <sdj1st11@gmail.com>
2023-11-09 07:04:26 +0900

    2023-11-09 이전 대화내용 기록 확인 가능 및 대화 내용 위치 변경

--- main.py
+++ main.py
@@ -1,61 +1,90 @@
-import openai
+import streamlit as st
+from db_connector import get_connection
 from dotenv import load_dotenv
 import os
-from db_connector import get_connection  # 데이터베이스 연결을 위한 모듈 import
 
 # .env 파일에서 환경 변수 로드
 load_dotenv()
 
-# OpenAI API 키 로드
-openai.api_key = os.getenv('OPENAI_API_KEY')
+# 초기화
+if 'search_history' not in st.session_state:
+    st.session_state['search_history'] = []
 
-def fetch_job_info_and_ask():
+def fetch_job_info_and_ask(column, user_question_keyword):
     # 데이터베이스에서 채용 정보 가져오기
     connection = get_connection()
     if not connection:
-        print("데이터베이스 연결에 실패했습니다.")
-        return
+        st.error("데이터베이스 연결에 실패했습니다.")
+        return []
 
     try:
         cursor = connection.cursor()
-        print("데이터베이스에 연결되었습니다, 쿼리를 실행합니다...")
-        user_question_keyword = '2022'  # 이 부분은 사용자의 입력에 따라 동적으로 바뀔 수 있습니다.
-        query = f"SELECT title, content, kind FROM job_postings WHERE title LIKE %s"
+        query = f"SELECT title, content, kind, organization, enddate FROM job_postings WHERE {column} LIKE %s"
         cursor.execute(query, ('%' + user_question_keyword + '%',))
-        
-        # 결과를 가져오는 동작 또한 try-except로 감싸주세요
-        try:
-            job_infos = cursor.fetchall()
-        except Exception as e:
-            print(f"결과를 가져오는 데 에러가 발생했습니다: {e}")
-            raise
-
-        print(f"쿼리가 실행되었습니다, {len(job_infos)}개의 행을 가져왔습니다.")
-
-        # job_infos 처리하는 코드
-        for job_info in job_infos:
-            if len(job_info) == 3:
-                title, content, kind = job_info
-                question = f"채용공고가 '{title}' 있나요? 있으면 자세한 설명 '{content}'과 모집 종류 '{kind}'도 같이 보여주세요."
-                
-                response = openai.Completion.create(
-                    engine="text-davinci-003", 
-                    prompt=question,
-                    max_tokens=1000
-                )
-                print(response.choices[0].text.strip())
-            else:
-                print("데이터베이스에서 채용공고를 찾을 수 없습니다.")
-
+        job_infos = cursor.fetchall()
     except Exception as e:
-        print(f"직업 정보를 가져오는 데 에러가 발생했습니다: {e}")
+        st.error(f"직업 정보를 가져오는 데 에러가 발생했습니다: {e}")
     finally:
-        # 닫기 전에 남아있는 모든 결과를 소비해야 합니다
-        while connection.unread_result:
-            cursor.fetchwarnings()
-            cursor.fetchall()
-        cursor.close()
-        connection.close()
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
 
-# 함수 실행
-fetch_job_info_and_ask()
+    return job_infos
+
+def display_job_info(job_info):
+    # 채용 정보를 문자열로 포맷팅하여 반환
+    title, content, kind, organization, enddate = job_info
+    info_str = f"**{title}**  \n" \
+           f"**채용처, 채용의뢰:** {organization}  \n" \
+           f"**마감일:** {enddate}  \n" \
+           f"**채용 유형:** {kind}  \n" \
+           f"**내용:** {content}"
+
+    st.markdown(info_str)
+               
+    return info_str
+
+def display_search_history():
+    # 검색 기록을 역순으로 표시
+    for entry in reversed(st.session_state['search_history']):
+        st.text(f"질문: {entry['question']} (검색 종류: {entry['column']})")
+        st.text("답변:")
+        st.write(entry['answer'])
+        st.write("---")
+
+def main():
+    st.title("채용공고 검색")
+
+    # 검색 기록 표시
+    display_search_history()
+
+    # 사용자 입력을 요청하는 UI 컴포넌트 (하단에 배치)
+    with st.container():
+        column = st.selectbox('검색할 열을 선택하세요:', ('title', 'organization', 'content', 'kind', 'enddate'), key='column_select')
+        user_question_keyword = st.text_input('검색하고 싶은 채용공고의 키워드를 입력하세요:', key='question_input')
+        search_clicked = st.button('검색')
+
+    # 검색 버튼 클릭 시 로직
+    if search_clicked:
+        job_infos = fetch_job_info_and_ask(column, user_question_keyword)
+
+        # 가져온 채용 정보 표시 및 검색 기록에 추가
+        if job_infos:
+            answer = ""
+            for job_info in job_infos[:3]:  # 최대 3개의 결과만 포맷팅
+                answer += display_job_info(job_info) + "\n\n---\n\n"
+            st.session_state['search_history'].append({
+                "column": column,
+                "question": user_question_keyword,
+                "answer": answer.rstrip("\n\n---\n\n")  # 마지막 구분선을 제거
+            })
+        else:
+            st.error("채용공고를 찾을 수 없습니다.")
+            st.session_state['search_history'].append({
+                "column": column,
+                "question": user_question_keyword,
+                "answer": "검색 결과가 없습니다."
+            })
+
+if __name__ == "__main__":
+    main()
