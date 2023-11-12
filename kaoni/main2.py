abc8b4c13b0ed260f109c91681bae207428fc2fd
손동진(DJSon) <sdj1st11@gmail.com>
2023-11-13 01:54:02 +0900

    main2 파일 최신 main 파일로 업데이트

--- kaoni/main2.py
+++ kaoni/main2.py
@@ -1,16 +1,39 @@
 import streamlit as st
 from db_connector import get_connection
+import openai
 from dotenv import load_dotenv
 import os
 
 # .env 파일에서 환경 변수 로드
 load_dotenv()
 
-# 초기화
-if 'search_history' not in st.session_state:
-    st.session_state['search_history'] = []
+# OpenAI API 키 로드
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def fetch_job_info_and_ask(column, user_question_keyword):
+# 페이지 설정
+st.set_page_config(page_title="채용공고 검색", layout="wide")
+
+
+# AI 모델을 사용하여 질문에서 키워드를 추출하는 함수
+def extract_keywords_from_question(question, model_name):
+    system_message = "너는 핵심 키워드를 구별하는 AI 모델이야"
+    full_prompt = {
+        "model": model_name,
+        "messages": [
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": question}
+        ],
+    }
+    try:
+        response = openai.ChatCompletion.create(**full_prompt)
+        keyword = response['choices'][0]['message']['content'].strip()
+        return keyword
+    except openai.error.OpenAIError as e:
+        st.error(f"키워드 추출 중 에러가 발생했습니다: {e}")
+        return None
+
+# 데이터베이스에서 채용 정보를 가져오는 함수
+def fetch_job_info(sql_query):
     # 데이터베이스에서 채용 정보 가져오기
     connection = get_connection()
     if not connection:
@@ -19,75 +42,120 @@ def fetch_job_info_and_ask(column, user_question_keyword):
 
     try:
         cursor = connection.cursor()
-        query = f"SELECT title, content, kind, organization, enddate FROM job_postings WHERE {column} LIKE %s"
-        cursor.execute(query, ('%' + user_question_keyword + '%',))
+        cursor.execute(sql_query)
         job_infos = cursor.fetchall()
+        
+        # 반환된 데이터 로깅
+        print(f"Fetched job infos: {job_infos}")
+        if job_infos and len(job_infos[0]) != 5:
+            print(f"Unexpected job info structure: {job_infos[0]}")
+        
+        return job_infos
     except Exception as e:
         st.error(f"직업 정보를 가져오는 데 에러가 발생했습니다: {e}")
+        return []
     finally:
-        if connection.is_connected():
+        if connection:
             cursor.close()
             connection.close()
 
-    return job_infos
-
+# 채용 정보를 표시하는 함수
 def display_job_info(job_info):
-    # 채용 정보를 문자열로 포맷팅하여 반환
-    title, content, kind, organization, enddate = job_info
-    info_str = f"**{title}**  \n" \
-           f"**채용처, 채용의뢰:** {organization}  \n" \
-           f"**마감일:** {enddate}  \n" \
-           f"**채용 유형:** {kind}  \n" \
-           f"**내용:** {content}"
+    try:
+        if len(job_info) == 5:
+            title, kind, organization, enddate, content = job_info
+            st.write(f"**{title}**")
+            st.write(f"**채용처, 채용의뢰:** {organization}")
+            st.write(f"**마감일:** {enddate}")
+            st.write(f"**채용 유형:** {kind}")
+            st.write(f"**내용:** {content}")
+            st.write(f"------------")
+        else:
+            st.error("채용 정보의 형식이 올바르지 않습니다.")
+    except Exception as e:
+        st.error(f"채용 정보를 표시하는 중 예외가 발생했습니다: {e}")
 
-    st.markdown(info_str)
+
+
+# 대화 기록을 저장할 전역 변수
+if 'conversation' not in st.session_state:
+    st.session_state['conversation'] = []
+
+# 사이드바에 사용자 정의 스타일을 적용하기 위한 CSS 로드 함수
+def local_css(file_name):
+    with open(file_name, "r", encoding="utf-8") as f:
+        st.sidebar.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# 사이드바 표시 함수
+def display_conversation_sidebar():
+    conversation = st.session_state['conversation']
+    conversation_text = ""
+    for item in conversation:
+        if item['type'] == "user":
+            conversation_text += f"<strong>사용자:</strong> {item['content']}<br>"
+            conversation_text += "<hr>"  # 대화 구분선
+        elif item['type'] == "answer":
+            conversation_text += f"<strong>AI 답변:</strong> {item['content']}<br>"
+        elif item['type'] == "job_info":
+            job_info = item['content']
+            title, kind, organization, enddate, content = job_info
+            conversation_text += f"<strong>{title}</strong><br>"
+            conversation_text += f"<strong>채용처, 채용의뢰:</strong> {organization}<br>"
+            conversation_text += f"<strong>마감일:</strong> {enddate}<br>"
+            conversation_text += f"<strong>채용 유형:</strong> {kind}<br>"
+            conversation_text += f"<strong>내용:</strong> {content}<br>"
+            conversation_text += "<hr>"  # 대화 구분선
                
-    return info_str
 
-def display_search_history():
-    # 검색 기록을 역순으로 표시하는 함수 안에서 expander를 사용
-    with st.expander("검색 기록 보기/숨기기"):
-        for entry in reversed(st.session_state['search_history']):
-            st.text(f"질문: {entry['question']} (검색 종류: {entry['column']})")
-            st.text("답변:")
-            st.write(entry['answer'])
-            st.write("---")
+    st.sidebar.markdown(conversation_text, unsafe_allow_html=True)
+
+# 대화 기록 제목을 스타일링
+st.sidebar.markdown('<div class="custom-header">대화 기록</div>', unsafe_allow_html=True)
 
 def main():
+
+    # CSS 파일 로드
+    local_css('style.css')
+
+    # 메인 페이지 컨텐츠
     st.title("채용공고 검색")
+    user_input = st.text_input("여기에 질문을 입력하세요:")
 
-    # 검색 기록 표시
-    display_search_history()
+    # '질문하기' 버튼이 클릭되면
+    if st.button("질문하기"):
+        
+        
+        # 사용자 입력을 대화 기록에 추가
+        st.session_state['conversation'].append({"type": "user", "content": user_input})
 
-    # 사용자 입력을 요청하는 UI 컴포넌트 (하단에 배치)
-    with st.container():
-        column = st.selectbox('검색할 열을 선택하세요:', ('title', 'organization', 'content', 'kind', 'enddate'), key='column_select')
-        user_question_keyword = st.text_input('검색하고 싶은 채용공고의 키워드를 입력하세요:', key='question_input')
-        search_clicked = st.button('검색')
+        # 파인 튜닝된 모델 이름 지정
+        model_name = "ft:gpt-3.5-turbo-0613:personal::8Jz0Zo0A"  # 모델 ID를 여기에 삽입
 
-    # 검색 버튼 클릭 시 로직
-    if search_clicked:
-        job_infos = fetch_job_info_and_ask(column, user_question_keyword)
+        # 질문에서 키워드 추출
+        extracted_keyword = extract_keywords_from_question(user_input, model_name)
 
-        # 가져온 채용 정보 표시 및 검색 기록에 추가
+        # 추출된 키워드가 에러 메시지를 포함하지 않는 경우
+        if extracted_keyword and 'Error' not in extracted_keyword:
+            # SQL 쿼리 생성
+            sql_query = f"SELECT title, kind, organization, enddate, content FROM job_postings WHERE title LIKE '%{extracted_keyword}%'"
+
+            # 데이터베이스에서 채용 정보 검색
+            job_infos = fetch_job_info(sql_query)
+
+        # 대화 기록에 검색 결과 추가
         if job_infos:
-            answer = ""
-            for job_info in job_infos[:3]:  # 최대 3개의 결과만 포맷팅
-                answer += display_job_info(job_info) + "\n\n---\n\n"
-            st.session_state['search_history'].append({
-                "column": column,
-                "question": user_question_keyword,
-                "answer": answer.rstrip("\n\n---\n\n")  # 마지막 구분선을 제거
-            })
+            # AI 답변을 대화 기록에 추가
+            st.session_state['conversation'].append({"type": "answer", "content": ""})
+            for job_info in job_infos:
+                display_job_info(job_info)
+                # 채용 정보를 대화 기록에 추가
+                st.session_state['conversation'].append({"type": "job_info", "content": job_info})
         else:
-            st.error("채용공고를 찾을 수 없습니다.")
-            st.session_state['search_history'].append({
-                "column": column,
-                "question": user_question_keyword,
-                "answer": "검색 결과가 없습니다."
-            })
+            st.error("해당 키워드에 맞는 채용공고를 찾을 수 없습니다.")
+
+    # 사이드바 표시 함수 호출
+    display_conversation_sidebar()
+    pass
 
 if __name__ == "__main__":
-    if 'search_history' not in st.session_state:
-        st.session_state['search_history'] = []  # 초기 검색 기록 상태 설정
     main()
